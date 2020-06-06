a7654a32c001e5f89d1fe33331528069ca23083a
Dmitry <dimk00z@gmail.com>
2020-06-06 22:01:43 +0300

    parse

--- dags/data_collector_dag.py
+++ dags/data_collector_dag.py
@@ -67,7 +67,6 @@ load_csv_op = PythonOperator(
 )
 
 
-
 def load_transactions_operations():
 
     url = 'https://api.jsonbin.io/b/5ed7391379382f568bd22822'
@@ -142,4 +141,59 @@ load_customers_op = PythonOperator(
 )
 
 
-load_csv_op >> load_transactions_operations_op >> load_goods_op >> load_customers_op
+def csv_dict_reader(file_name, key_field):
+    result_table = {}
+    with open(file_name) as file_obj:
+        reader = csv.DictReader(file_obj, delimiter=',')
+        for line in reader:
+            result_table[line[key_field]] = line
+    return result_table
+
+# Финальный датасет содержит следующие колонки:
+# name, age, good_title, date, payment_status, total_price, amount, last_modified_at.
+# name => customers, orders
+# age => customers
+# good_title => goods
+# amount => goods, orders
+#  last_modified_at => orders
+
+
+def save_data():
+
+    transactions = csv_dict_reader(TRANSACTIONS_FILE_NAME, 'transaction_uuid')
+    goods = csv_dict_reader(GOODS_FILE_NAME, 'name')
+    orders = csv_dict_reader(ORDERS_FILE_NAME, 'uuid заказа')
+    customers = csv_dict_reader(CUSTOMERS_FILE_NAME, 'email')
+
+    result_data_set = []
+    for uuid_order, order in orders.items():
+        customer_email = order['email']
+        customer_age = customers[customer_email]['birth_date']
+        customer_name = customers[customer_email]['name']
+        last_modified_at = order['дата заказа']
+        good_title = order['название товара']
+        date = order['дата заказа']
+        amount = int(order['количество'])
+        total_price = round(amount * float(goods[good_title]['price']), 2)
+        payment_status = transactions[uuid_order]['transaction_status']
+        print(customer_name, last_modified_at,
+              amount, good_title, customer_age, payment_status, total_price, date)
+        break
+        # customer_name = order['ФИО']
+        # last_modified_at = order['дата заказа']
+        # amount = order['количество']
+        # good_title = order['good_title']
+
+    # print(len(transactions))
+    # print(len(goods))
+    # print(len(orders))
+    # print(len(customers))
+
+
+save_data_op = PythonOperator(
+    task_id='save_data_op',
+    python_callable=save_data,
+    dag=dag,
+)
+
+load_csv_op >> load_transactions_operations_op >> load_goods_op >> load_customers_op >> save_data_op
